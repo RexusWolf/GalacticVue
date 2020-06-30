0d798db49fa8b6cb814c7b5f37a8605c336a3dd0
rexuswolf <francisjmp99@gmail.com>
2020-06-30 13:39:38 +0200

    Working on cardBack data

--- src/components/GalacticVCardBack.vue
+++ src/components/GalacticVCardBack.vue
@@ -1,41 +1,9 @@
 <template>
   <v-card hover class="mx-auto mb-5 pt-2" max-width="290" :style="cssProps">
     <v-container fluid class="starJediFont pl-7 pb-0">
-      <v-row class="mb-2">
-        <h3 class="white--text">Height:</h3>
-        <h3 class="ml-2 white--text">1,72</h3>
-      </v-row>
-      <v-row class="mb-2">
-        <h3 class="white--text">Mass:</h3>
-        <h3 class="ml-2 white--text">77</h3>
-      </v-row>
-      <v-row class="mb-2">
-        <h3 class="white--text">Hair Color:</h3>
-        <h3 class="ml-2 white--text">Blond</h3>
-      </v-row>
-      <v-row class="mb-2">
-        <h3 class="white--text">Skin Color:</h3>
-        <h3 class="ml-2 white--text">Fair</h3>
-      </v-row>
-      <v-row class="mb-2">
-        <h3 class="white--text">Eye Color:</h3>
-        <h3 class="ml-2 white--text">Blue</h3>
-      </v-row>
-      <v-row class="mb-2">
-        <h3 class="white--text">Birth Year:</h3>
-        <h3 class="ml-2 white--text">19BBY</h3>
-      </v-row>
-      <v-row class="mb-2">
-        <h3 class="white--text">Gender:</h3>
-        <h3 class="ml-2 white--text">Male</h3>
-      </v-row>
-      <v-row class="mb-2">
-        <h3 class="white--text">Homeworld:</h3>
-        <h3 class="ml-2 white--text">Tatooine</h3>
-      </v-row>
-      <v-row class="mb-2">
-        <h3 class="white--text">Starship:</h3>
-        <h3 class="ml-2 white--text">X-Wing</h3>
+      <v-row class="mb-2" v-bind:key="prop" v-for="prop in props">
+        <h3 class="white--text">{{prop[0].replace(/([A-Z])/g, ' $1').trim()}}:</h3>
+        <h3 class="ml-2 white--text">{{prop[1]}}</h3>
       </v-row>
     </v-container>
     <v-card-actions class>
@@ -48,31 +16,50 @@
 export default {
   name: "GalacticVCardBack",
   props: {
-    cardTitle: {
+    height: {
       type: String,
       required: true,
-      default: "Anakin Skywalker"
+      default: "1,77"
     },
-    cardSubtitle: {
+    mass: {
       type: String,
       required: true,
-      default: "JEDI KNIGHT"
+      default: "77"
     },
-    cardText: {
+    hairColor: {
       type: String,
       required: true,
-      default:
-        "Anakin Skywalker, a Force-sensitive human male, was a Jedi Knight of the Galactic Republic and the Chosen One of the Force."
+      default: "Blonde"
     },
-    forceColor: {
+    skinColor: {
       type: String,
       required: true,
-      default: "deepskyblue"
+      default: "Fair"
     },
-    infoUrl: {
+    eyeColor: {
       type: String,
       required: true,
-      default: "https://starwars.fandom.com/wiki/Anakin_Skywalker"
+      default: "Blue"
+    },
+    birthYear: {
+      type: String,
+      required: true,
+      default: "19BBY"
+    },
+    gender: {
+      type: String,
+      required: true,
+      default: "Male"
+    },
+    homeworld: {
+      type: String,
+      required: true,
+      default: "Tattooine"
+    },
+    starship: {
+      type: String,
+      required: true,
+      default: "X-wing"
     }
   },
   data: () => ({}),
@@ -81,6 +68,9 @@ export default {
       return {
         "--forceColor": this.forceColor
       };
+    },
+    props() {
+      return Object.entries(this.$props);
     }
   }
 };
