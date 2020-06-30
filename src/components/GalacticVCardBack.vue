22ca4b76c59a23542ab8a3e544a77eba61b1d4b9
rexuswolf <francisjmp99@gmail.com>
2020-06-30 15:40:53 +0200

    Redesigning stuff

--- src/components/GalacticVCardBack.vue
+++ src/components/GalacticVCardBack.vue
@@ -1,14 +1,11 @@
 <template>
-  <v-card hover class="mx-auto mb-5 pt-2" max-width="290" :style="cssProps">
-    <v-container fluid class="starJediFont pl-7 pb-0">
+  <v-card hover class="mx-auto mb-5 pt-4" max-width="290" :style="cssProps">
+    <v-container fluid class="starJediFont pl-8">
       <v-row class="mb-2" v-bind:key="index" v-for="(key, index) in keys">
-        <h3 class="white--text">{{key}}:</h3>
-        <h3 class="ml-2 white--text">{{values[index]}}</h3>
+        <h3 class="gvyellow--text">{{key}}:</h3>
+        <h3 class="ml-2">{{values[index]}}</h3>
       </v-row>
     </v-container>
-    <v-card-actions class>
-      <v-btn :href="this.infoUrl" target="_blank" text class="cardButton starJediFont">READ MORE</v-btn>
-    </v-card-actions>
   </v-card>
 </template>
 
@@ -16,49 +13,44 @@
 export default {
   name: "GalacticVCardBack",
   props: {
+    species: {
+      type: String,
+      default: "Human"
+    },
     height: {
       type: String,
-      required: true,
       default: "1,77"
     },
     mass: {
       type: String,
-      required: true,
       default: "77"
     },
     hairColor: {
       type: String,
-      required: true,
       default: "Blonde"
     },
     skinColor: {
       type: String,
-      required: true,
       default: "Fair"
     },
     eyeColor: {
       type: String,
-      required: true,
       default: "Blue"
     },
     birthYear: {
       type: String,
-      required: true,
       default: "19BBY"
     },
     gender: {
       type: String,
-      required: true,
       default: "Male"
     },
     homeworld: {
       type: String,
-      required: true,
-      default: "Tattooine"
+      default: "Tatooine"
     },
     starship: {
       type: String,
-      required: true,
       default: "X-wing"
     },
     forceColor: {
