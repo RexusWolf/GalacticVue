d240df75b54d68d32d15caf9a6ab77ffcf501523
rexuswolf <francisjmp99@gmail.com>
2020-07-13 12:00:38 +0200

    Sidebar mini-variant fixed

--- src/components/GalacticVSidebar.vue
+++ src/components/GalacticVSidebar.vue
@@ -6,12 +6,21 @@
     :mini-variant.sync="mini"
     permanent
   >
-    <v-list>
-      <v-list-item>
-        <v-btn right fixed icon @click.stop="mini = !mini">
-          <v-icon>mdi-chevron-left</v-icon>
-        </v-btn>
-      </v-list-item>
+    <v-list-item class="px-2">
+      <v-list-item-avatar>
+        <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+      </v-list-item-avatar>
+
+      <v-list-item-title>{{this.username}}</v-list-item-title>
+
+      <v-btn icon @click.stop="mini = !mini">
+        <v-icon>mdi-chevron-left</v-icon>
+      </v-btn>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-list dense>
       <v-list-item v-for="item in items" :key="item.title" link>
         <v-list-item-icon>
           <v-icon>{{ item.icon }}</v-icon>
@@ -49,6 +58,10 @@ export default {
     sithTheme: {
       type: Boolean,
       default: false
+    },
+    username: {
+      type: String,
+      default: "rexuswolf"
     }
   },
   data() {
