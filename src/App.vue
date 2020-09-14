77f9b60a739fd3ea66445b875c06af30e78209e2
rexuswolf <francisjmp99@gmail.com>
2020-09-14 13:06:04 +0200

    Making this prettier

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,44 @@
 <template>
   <v-app>
     <galactic-v-nav-bar sidebar v-bind:theme="selectedTheme"></galactic-v-nav-bar>
-    <v-content class="mx-auto pt-5">
-      <v-select class="mx-4" placeholder="Select theme..." v-model="selectedTheme" :items="themes"></v-select>
-      <v-row>
-        <flip-galactic-v-card class="ml-5 mr-5">
-          <template slot="front">
-            <galactic-v-card></galactic-v-card>
-          </template>
-          <template slot="back">
-            <galactic-v-card-back
-              forceColor="deepskyblue"
-              backImageUrl="https://i.pinimg.com/originals/37/85/73/37857333246ddda79c349dd0aa500a01.jpg"
-            ></galactic-v-card-back>
-          </template>
-        </flip-galactic-v-card>
-        <flip-galactic-v-card class="ml-5 mr-5">
-          <template slot="front">
-            <galactic-v-card
-              cardTitle="Darth Maul"
-              cardSubtitle="SITH"
-              cardImageUrl="https://sm.ign.com/t/ign_latam/screenshot/default/darthmaul-1_a2ev.1280.jpg"
-              cardText="Maul was a Force-sensitive Dathomirian Zabrak male and dark side warrior who served as the apprentice Dark Lord of the Sith Darth Maul during the final years of the Galactic Republic and reigned as a crime lord during the rule of the Galactic Empire. "
-              forceColor="red"
-              infoUrl="https://starwars.fandom.com/wiki/Maul"
-            ></galactic-v-card>
-          </template>
-          <template slot="back">
-            <galactic-v-card-back
-              forceColor="red"
-              backImageUrl="https://i.pinimg.com/originals/e7/a0/55/e7a0550a21ffc8933f26526a57254265.jpg"
-            ></galactic-v-card-back>
-          </template>
-        </flip-galactic-v-card>
-        <flip-galactic-v-card class="ml-5">
-          <template slot="front">
-            <galactic-v-card
-              cardTitle="Luke Skywalker"
-              cardSubtitle="JEDI MASTER"
-              cardImageUrl="https://dreager1.files.wordpress.com/2010/11/lukeskywalkerwallpaper-777357.jpg"
-              cardText="Luke Skywalker, a Force-sensitive human male, was a legendary Jedi Master who fought in the Galactic Civil War during the reign of the Galactic Empire.Luke Skywalker, a Force-sensitive human male, was a legendary Jedi Master who fought in the Galactic Civil War during the reign of the Galactic Empire. "
-              forceColor="lawngreen"
-              infoUrl="https://starwars.fandom.com/wiki/Luke_Skywalker"
-            ></galactic-v-card>
-          </template>
-          <template slot="back">
-            <galactic-v-card-back
-              forceColor="lawngreen"
-              backImageUrl="https://img.vixdata.io/pd/jpg-large/es/sites/default/files/btg/curiosidades.batanga.com/files/Segun-la-ciencia-la-princesa-Leia-es-mayor-que-Luke-Skywalker-a-pesar-de-ser-su-hermana-melliza-0.jpeg"
-            ></galactic-v-card-back>
-          </template>
-        </flip-galactic-v-card>
-      </v-row>
-      <v-row class="mx-auto">
-        <galactic-v-button outlined theme="originalTheme">ORIGINAL OUTLINED</galactic-v-button>
-        <galactic-v-button theme="cloneTheme">CLONES NOT OUTLINED</galactic-v-button>
-        <galactic-v-button outlined theme="sithTheme">SITH</galactic-v-button>
-        <galactic-v-button theme="empireTheme">EMPIRE</galactic-v-button>
-      </v-row>
-    </v-content>
+    <v-main>
+      <v-container fill-height>
+        <v-row justify="center">
+          <v-col cols="6">
+            <div class="text-h4 text-center">Actual theme is: {{this.selectedTheme}}</div>
+            <v-select placeholder="Select theme..." v-model="selectedTheme" :items="themes"></v-select>
+          </v-col>
+          <v-col cols="12" class="d-flex justify-space-around">
+            <v-col cols="12" sm="6" md="3" v-for="(card,index) in this.cards" :key="index">
+              <flip-galactic-v-card>
+                <template slot="front">
+                  <galactic-v-card
+                    :cardTitle="card.cardTitle"
+                    :cardSubtitle="card.cardSubtitle"
+                    :cardImageUrl="card.cardImageUrl"
+                    :cardText="card.cardText"
+                    :forceColor="card.forceColor"
+                    :infoUrl="card.infoUrl"
+                  ></galactic-v-card>
+                </template>
+                <template slot="back">
+                  <galactic-v-card-back
+                    :forceColor="card.forceColor"
+                    :backImageUrl="card.backImageUrl"
+                  ></galactic-v-card-back>
+                </template>
+              </flip-galactic-v-card>
+            </v-col>
+          </v-col>
+          <v-col cols="12" class="d-flex justify-center">
+            <galactic-v-button outlined theme="originalTheme">ORIGINAL OUTLINED</galactic-v-button>
+            <galactic-v-button theme="cloneTheme">CLONES NOT OUTLINED</galactic-v-button>
+            <galactic-v-button outlined theme="sithTheme">SITH</galactic-v-button>
+            <galactic-v-button theme="empireTheme">EMPIRE</galactic-v-button>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
@@ -73,18 +53,50 @@ export default {
   name: "App",
   props: {
     selectedTheme: {
-      type: String
-    }
+      type: String,
+      default: "originalTheme",
+    },
   },
   components: {
     FlipGalacticVCard,
     GalacticVCard,
     GalacticVCardBack,
     GalacticVButton,
-    GalacticVNavBar
+    GalacticVNavBar,
   },
   data: () => ({
-    themes: ["originalTheme", "cloneTheme", "sithTheme", "empireTheme"]
-  })
+    themes: ["originalTheme", "cloneTheme", "sithTheme", "empireTheme"],
+    cards: [
+      {
+        cardTitle: "Darth Maul",
+        cardSubtitle: "SITH",
+        cardImageUrl:
+          "https://sm.ign.com/t/ign_latam/screenshot/default/darthmaul-1_a2ev.1280.jpg",
+        cardText:
+          "Maul was a Force-sensitive Dathomirian Zabrak male and dark side warrior who served as the apprentice Dark Lord of the Sith Darth Maul during the final years of the Galactic Republic and reigned as a crime lord during the rule of the Galactic Empire. ",
+        forceColor: "red",
+        infoUrl: "https://starwars.fandom.com/wiki/Maul",
+        backImageUrl:
+          "https://i.pinimg.com/originals/e7/a0/55/e7a0550a21ffc8933f26526a57254265.jpg",
+      },
+      {
+        forceColor: "deepskyblue",
+        backImageUrl:
+          "https://i.pinimg.com/originals/37/85/73/37857333246ddda79c349dd0aa500a01.jpg",
+      },
+      {
+        cardTitle: "Luke Skywalker",
+        cardSubtitle: "JEDI MASTER",
+        cardImageUrl:
+          "https://dreager1.files.wordpress.com/2010/11/lukeskywalkerwallpaper-777357.jpg",
+        cardText:
+          "Luke Skywalker, a Force-sensitive human male, was a legendary Jedi Master who fought in the Galactic Civil War during the reign of the Galactic Empire.Luke Skywalker, a Force-sensitive human male, was a legendary Jedi Master who fought in the Galactic Civil War during the reign of the Galactic Empire. ",
+        forceColor: "lawngreen",
+        infoUrl: "https://starwars.fandom.com/wiki/Luke_Skywalker",
+        backImageUrl:
+          "https://img.vixdata.io/pd/jpg-large/es/sites/default/files/btg/curiosidades.batanga.com/files/Segun-la-ciencia-la-princesa-Leia-es-mayor-que-Luke-Skywalker-a-pesar-de-ser-su-hermana-melliza-0.jpeg",
+      },
+    ],
+  }),
 };
 </script>
