56c6a3fe9c34e489e9e0e075b4359d484463038c
rexuswolf <francisjmp99@gmail.com>
2020-12-20 13:53:40 +0100

    Dotenv config

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import firebase from 'firebase';
-import { firebaseConfig } from './.env'
+import dotenv from 'dotenv'
+dotenv.config()
+
 Vue.config.productionTip = false
+
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+}
 
 new Vue({
   vuetify,
